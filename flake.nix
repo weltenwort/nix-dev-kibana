05071abd5917224b69d9e2c9f7fbaea6f51181cf
Felix Stürmer <stuermer@weltenwort.de>
2023-09-05 15:38:07 +0200

    add initial content

---
+++ flake.nix
@@ -0,0 +1,166 @@
+{
+  description = "A Kibana development flake";
+  inputs.nixpkgs = {
+    url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    #url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        makePkgs = { overlays ? [ ] }:
+          import nixpkgs {
+            inherit system;
+            overlays = [
+              (final: prev: {
+                nodejs-current = prev.nodejs-16_x;
+              })
+            ] ++ overlays ++
+            [
+              (final: prev: {
+                yarn = prev.yarn.override {
+                  nodejs = final.nodejs-current;
+                };
+                chromium = prev.symlinkJoin {
+                  name = "chromium";
+                  paths = [ prev.chromium ];
+                  buildInputs = [ final.makeWrapper ];
+                  postBuild = ''
+                    wrapProgram $out/bin/chromium \
+                    --add-flags "--disable-gpu"
+                  '';
+                };
+              })
+            ];
+          };
+        makeCommonPackages = { pkgs }: [
+          pkgs.nodejs-current
+          pkgs.yarn
+          pkgs.chromium
+          pkgs.fontconfig
+          pkgs.noto-fonts
+          pkgs.icewm
+        ];
+        makeScripts = { pkgs }: [
+          (pkgs.writeShellApplication {
+            name = "kbn-start-xvnc";
+            runtimeInputs = [
+              pkgs.tigervnc
+            ];
+            text = ''
+              Xvnc -depth 24 -geometry 1920x1080 -rfbport 5900 SecurityTypes=None
+            '';
+          })
+          (pkgs.writeShellApplication {
+            name = "kbn-start-xpack-functional-server";
+            text = ''
+              set -x
+              cd x-pack && node scripts/functional_tests_server.js --config "''${1:-test/functional/config.js}"
+            '';
+          })
+          (pkgs.writeShellApplication {
+            name = "kbn-start-functional-runner";
+            text = ''
+              set -x
+              node scripts/functional_test_runner.js --config "''${1:-x-pack/test/functional/config.js}"
+            '';
+          })
+          (pkgs.writeShellApplication {
+            name = "kbn-bootstrap";
+            text = ''
+              set -x
+              node scripts/kbn.js bootstrap && NODE_OPTIONS=--openssl-legacy-provider node scripts/build_kibana_platform_plugins.js --no-examples
+            '';
+          })
+          (pkgs.writeShellApplication {
+            name = "kbn-start-dev";
+            text = ''
+              set -x
+              NODE_OPTIONS=--openssl-legacy-provider node scripts/kibana.js --dev
+            '';
+          })
+        ];
+        makeCommonShell = { pkgs }: {
+          DISPLAY = ":0";
+          FONTCONFIG_FILE = "${pkgs.fontconfig.out}/etc/fonts/fonts.conf";
+        };
+      in
+      {
+        devShells = {
+          metaDev =
+            let
+              pkgs = makePkgs { };
+            in
+            pkgs.mkShell {
+              packages = [
+                pkgs.nil
+                pkgs.nixpkgs-fmt
+              ];
+            };
+          meta =
+            let
+              pkgs = makePkgs { };
+              scripts = (pkgs.writeShellApplication {
+                name = "meta-clone";
+                runtimeInputs = [
+                  pkgs.git
+                  pkgs.gh
+                ];
+                text = ''
+                  cd ~/repos
+                  gh repo clone weltenwort/kibana
+                  echo "use flake ~/nix-flakes/kibana#main" >> ~/repos/kibana/.envrc
+                '';
+              });
+              commonShell = makeCommonShell { inherit pkgs; };
+            in
+            pkgs.mkShell commonShell // {
+              packages = scripts;
+            };
+          main =
+            let
+              pkgs = makePkgs {
+                overlays = [
+                  #            (final: prev: import ./nodejs-versions.nix {
+                  #              pkgs = prev;
+                  #              inherit nixpkgs;
+                  #            })
+                  #            (final: prev: {
+                  #              nodejs-current = final.nodejs-16_16_0;
+                  #            })
+                ];
+              };
+              scripts = makeScripts { inherit pkgs; };
+              commonPackages = makeCommonPackages { inherit pkgs; };
+              commonShell = makeCommonShell { inherit pkgs; };
+            in
+            pkgs.mkShell commonShell // {
+              packages = commonPackages ++ scripts;
+            };
+          v7-16 =
+            let
+              pkgs = makePkgs {
+                overlays = [
+                  (final: prev: import ./nodejs-versions.nix {
+                    pkgs = prev;
+                    inherit nixpkgs;
+                  })
+                  (final: prev: {
+                    nodejs-current = final.nodejs-16_13_0;
+                  })
+                ];
+              };
+              scripts = makeScripts { inherit pkgs; };
+              commonShell = makeCommonShell { inherit pkgs; };
+              commonPackages = makeCommonPackages { inherit pkgs; };
+            in
+            pkgs.mkShell commonShell // {
+              packages = commonPackages ++ scripts;
+            };
+        };
+      });
+}
---
+++ nodejs-versions.nix
@@ -0,0 +1,39 @@
+{ pkgs, nixpkgs }:
+let
+  buildNodejs = pkgs.callPackage (nixpkgs + "/pkgs/development/web/nodejs/nodejs.nix") {
+    openssl = pkgs.openssl;
+    python = pkgs.python3;
+  };
+in {
+  nodejs-16_13_0 = buildNodejs {
+    enableNpm = true;
+    version = "16.13.0";
+    sha256 = "1k6bgs83s5iaawi63dcc826g23lfqr13phwbbzwx0pllqcyln49j";
+  };
+  nodejs-16_13_2 = buildNodejs {
+    enableNpm = true;
+    version = "16.13.2";
+    sha256 = "185lm13q0kwz0qimc38c7mxn8ml6m713pjdjsa9jna9az4gxxccq";
+    patches = [
+      (pkgs.fetchpatch {
+        url = "https://github.com/nodejs/node/commit/65119a89586b94b0dd46b45f6d315c9d9f4c9261.patch";
+        sha256 = "sha256-dihKYEdK68sQIsnfTRambJ2oZr0htROVbNZlFzSAL+I=";
+      })
+    ];
+  };
+  nodejs-16_16_0 = buildNodejs {
+    enableNpm = true;
+    version = "16.16.0";
+    sha256 = "sha256-FFFR7/Oyql6+czhACcUicag3QK5oepPJjGKM19UnNus=";
+    patches = [
+      # Fix npm silently fail without a HOME directory https://github.com/npm/cli/issues/4996
+      (pkgs.fetchpatch {
+        url = "https://github.com/npm/cli/commit/9905d0e24c162c3f6cc006fa86b4c9d0205a4c6f.patch";
+        sha256 = "sha256-RlabXWtjzTZ5OgrGf4pFkolonvTDIPlzPY1QcYDd28E=";
+        includes = [ "deps/npm/lib/npm.js" "deps/npm/lib/utils/log-file.js" ];
+        stripLen = 1;
+        extraPrefix = "deps/npm/";
+      })
+    ];
+  };
+}
