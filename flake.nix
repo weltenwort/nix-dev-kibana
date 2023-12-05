aaa36cec8314366c0c1b8bec2b3da08e267c2735
Felix Stürmer <weltenwort@users.noreply.github.com>
2023-12-05 18:06:13 +0100

    Add more helper scripts

--- flake.nix
+++ flake.nix
@@ -81,6 +81,13 @@
             '';
           })
           (pkgs.writeShellApplication {
+            name = "kbn-start-serverless-oblt-dev";
+            text = ''
+              set -x
+              NODE_OPTIONS=--openssl-legacy-provider node scripts/kibana.js --dev --no-base-path --serverless=oblt
+            '';
+          })
+          (pkgs.writeShellApplication {
             name = "kbn-lint-fix";
             text = ''
               set -x
@@ -118,6 +125,28 @@
                     cd ~/repos
                     gh repo clone weltenwort/kibana
                     echo "use flake ~/nix-flakes/kibana#main" >> ~/repos/kibana/.envrc
+                  '';
+                })
+                (pkgs.writeShellApplication {
+                  name = "meta-install-docker";
+                  text = ''
+                    # Add Docker's official GPG key:
+                    sudo apt-get update
+                    sudo apt-get install ca-certificates curl gnupg
+                    sudo install -m 0755 -d /etc/apt/keyrings
+                    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+                    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+                    # Add the repository to Apt sources:
+                    # shellcheck source=/dev/null
+                    echo \
+                      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+                      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+                      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+                    sudo apt-get update
+
+                    # Install packages
+                    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
                   '';
                 })
               ];
